5b32cb4f4d2381f8c225165f9911c6b1bfb6c717
Imma7 <immamugambi@gmail.com>
2020-03-05 12:26:45 +0300

    Modify database engine

--- listing/settings.py
+++ listing/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'listing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'propertylist',
         'USER': 'imma',
         'PASSWORD': '7007',
