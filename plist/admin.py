5f15cc5224240c85b8eb88c84fc29e70f9196406
Imma7 <immamugambi@gmail.com>
2020-03-06 15:10:26 +0300

    Create Image Inline and PropertyAdmin

--- plist/admin.py
+++ plist/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin 
+from . models import Image
 
 # Register your models here.
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 10
+
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [ImageInline]
