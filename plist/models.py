dda9698c193ddc199afcd2e1e8be77547ceec9aa
Imma7 <immamugambi@gmail.com>
2020-03-06 14:47:05 +0300

    Ordering of object of Property using class meta created on

--- plist/models.py
+++ plist/models.py
@@ -8,6 +8,10 @@ class Property (models.Model):
     price = models.DecimalField(max_digits=10)
     contact = models.CharField(max_length=13)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ['-created_at'] 
 
     def __str__(self):
         return self.property_name
