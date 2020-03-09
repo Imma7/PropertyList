a127570f550d884d112ebec9a502b7015fc547ea
Imma7 <immamugambi@gmail.com>
2020-03-09 15:32:13 +0300

    Customize Property Admin with list filter, list display and serach fields

--- plist/admin.py
+++ plist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from . models import Image
+from . models import Image, Property, Category, Location
 
 # Register your models here.
 class ImageInline(admin.StackedInline):
@@ -7,4 +7,14 @@ class ImageInline(admin.StackedInline):
     extra = 10
 
 class PropertyAdmin(admin.ModelAdmin):
+    # Model Admin field Options
+    list_display = ('property_name', 'description', 'location', 'price', 'category', 'created_at')
+    list_filter = ('property_name', 'location', 'price', 'category')
+    search_fields = ('property_name',)
     inlines = [ImageInline]
+    
+
+admin.site.register(Image)
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Location)
+admin.site.register(Category)
