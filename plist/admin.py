048f45830fe95c4d5964ea807caf1597910398ac
Imma7 <immamugambi@gmail.com>
2020-03-10 12:02:51 +0300

    Register PropertyType model

--- plist/admin.py
+++ plist/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from . models import Image, Property, Category, Location
+from . models import Image, Property, ListingType, PropertyType, Location
 
 # Register your models here.
 class ImageInline(admin.StackedInline):
@@ -8,8 +8,8 @@ class ImageInline(admin.StackedInline):
 
 class PropertyAdmin(admin.ModelAdmin):
     # Model Admin field Options
-    list_display = ('property_name', 'description', 'location', 'price', 'category', 'created_at')
-    list_filter = ('property_name', 'location', 'price', 'category')
+    list_display = ('property_name', 'description', 'location', 'price', 'listings', 'properties', 'created_at')
+    list_filter = ('property_name', 'location', 'price', 'listings', 'properties')
     search_fields = ('property_name',)
     inlines = [ImageInline]
     
@@ -17,4 +17,5 @@ class PropertyAdmin(admin.ModelAdmin):
 admin.site.register(Image)
 admin.site.register(Property, PropertyAdmin)
 admin.site.register(Location)
-admin.site.register(Category)
+admin.site.register(ListingType)
+admin.site.register(PropertyType)
